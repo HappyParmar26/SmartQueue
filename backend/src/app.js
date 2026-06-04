b407bdc99d2609ed21f7851c636ad691954b92a9
HappyParmar26 <happyparmar2006@gmail.com>
2026-06-04 17:48:05 +0530

    officeController

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const officeRouter = require('./routes/office.routes');
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/v1/citizen/offices", officeRouter)
 
 module.exports = app;

4dc5406288a9014e558f834e8d346d87d6547992
Subhranshu Jha <jhasubhranshu2005@gmail.com>
2026-06-05 17:00:17 +0530

    done with the prediction route features

--- ml_service/app/main.py
+++ ml_service/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+from routes import prediction_route
 
 app = FastAPI()
 
@@ -6,3 +8,11 @@ app = FastAPI()
 def root():
     return { "success": True, "message": "ML Service health Check" }
 
+app.include_router(
+    prediction_route.prediction_router,
+    prefix="/api/v1",
+    tags=["Prediction"]
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main.app", host="0.0.0.0", port=8000 , reload=True)
